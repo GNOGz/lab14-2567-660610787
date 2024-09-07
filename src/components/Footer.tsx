d9fa295fee31739d017067e1374a185a3eba8676
GNOGz <99389733+GNOGz@users.noreply.github.com>
2024-09-07 20:04:05 +0700

    done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div>
-      <p>Copyright ©</p>
+      <p>Copyright © 2024 Yutthakarn Sajui 660610787</p>
     </div>
   );
 }
--- src/lib/types.tsx
+++ src/lib/types.tsx
@@ -2,3 +2,8 @@ export interface TermsAndConditionsProps {
   opened: boolean;
   close: () => void;
 }
+export interface footercomponent {
+  year: string;
+  fullName: string;
+  studentId: string;
+}
